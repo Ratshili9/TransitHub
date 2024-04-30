9ccaa68254d39907173d8ec7594f5eacedd6bd6d
Jackonia <99131662+Ratshili9@users.noreply.github.com>
2024-04-30 18:12:05 +0200

    fluter prototype(roadmaster_prototype)...

--- RouteMaster/App.js
+++ RouteMaster/App.js
@@ -1,31 +1,140 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
+import * as SplashScreen from "expo-splash-screen";
+import * as Updates from "expo-updates";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  Animated,
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// Instruct SplashScreen not to hide yet, we want to do this manually
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 
-
-import OnboardingScreen from './screens/authentication/OnboardingScreen';
-import LoginScreen from './screens/authentication/LoginScreen';
-import RegisterScreen from './screens/authentication/RegisterScreen';
-import ForgotPasswordScreen from './screens/authentication/ForgotPasswordScreen';
-import ResetPasswordScreen from './screens/authentication/ResetPasswordScreen';
-
-const Stack = createStackNavigator();
-
-function App() {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-        <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AnimatedAppLoader image={{ uri: Constants.expoConfig.splash.image }}>
+      <MainScreen />
+    </AnimatedAppLoader>
   );
 }
 
-export default App;
+function AnimatedAppLoader({ children, image }) {
+  const [isSplashReady, setSplashReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await Asset.fromURI(image.uri).downloadAsync();
+      setSplashReady(true);
+    }
+
+    prepare();
+  }, [image]);
+
+  if (!isSplashReady) {
+    return null;
+  }
+
+  return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>;
+}
+
+function AnimatedSplashScreen({ children, image }) {
+  const animation = useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = useCallback(async () => {
+    try {
+      await SplashScreen.hideAsync();
+      // Load stuff
+      await Promise.all([]);
+    } catch (e) {
+      // handle errors
+    } finally {
+      setAppReady(true);
+    }
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.expoConfig.splash.backgroundColor,
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: Constants.expoConfig.splash.resizeMode || "contain",
+              transform: [
+                {
+                  scale: animation,
+                },
+              ],
+            }}
+            source={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+function MainScreen() {
+  const onReloadPress = useCallback(() => {
+    if (Platform.OS === "web") {
+      location.reload();
+    } else {
+      Updates.reloadAsync();
+    }
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "plum",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          color: "black",
+          fontSize: 30,
+          marginBottom: 15,
+          fontWeight: "bold",
+        }}
+      >
+        Pretty Cool!
+      </Text>
+      <Button title="Run Again" onPress={onReloadPress} />
+    </View>
+  );
+}
